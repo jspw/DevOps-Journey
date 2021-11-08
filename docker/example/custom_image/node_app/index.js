79ce1159f9d523d26bad08340c890f24b5d2983f
jspw <mhshifat757@gmail.com>
2021-11-08 18:25:33 +0600

    docker tutorial finishied

---
+++ docker/example/custom_image/node_app/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json([
+    {
+      message: "Hello World!",
+    },
+    {
+      message: "No World!",
+    },
+    {
+      message: "Hello World!",
+    },
+    {
+      message: "Hello World!",
+    },
+    {
+      message: "No World!",
+    },
+    {
+      message: "Hello World!",
+    },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
